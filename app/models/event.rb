edb446adcf299ca484ffc5f833ad4aabdee15943
David McNally <david.mcnally@artofsource.com>
2026-01-18 17:34:57 +0000

    Use action cable to update the timeline

--- app/models/event.rb
+++ app/models/event.rb
@@ -7,4 +7,18 @@ class Event < ApplicationRecord
   has_many :details, class_name: "Event::Detail", dependent: :destroy
 
   validates :action, presence: true
+
+  after_create_commit :broadcast_timeline_append
+
+  private
+
+  def broadcast_timeline_append
+    root_recording = eventable.root
+    Turbo::StreamsChannel.broadcast_append_to(
+      root_recording, "timeline",
+      target: ActionView::RecordIdentifier.dom_id(root_recording, :timeline_events),
+      partial: "timelines/event",
+      locals: { event: self }
+    )
+  end
 end
